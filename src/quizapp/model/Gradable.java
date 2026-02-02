8eda9867e6be4af6b939cf72a7c2a007cf1470f6
Huawei <Huawei@Huawei>
2026-02-02 13:36:20 +0300

    Reintroduce Gradable interface and implement it in Question

---
+++ src/quizapp/model/Gradable.java
@@ -0,0 +1,14 @@
+package quizapp.model;
+
+/**
+ * Puan bilgisi olan nesneler için basit arayüz.
+ */
+public interface Gradable {
+
+    /**
+     * Nesnenin puan değerini döndürür.
+     *
+     * @return puan
+     */
+    int getPoints();
+}
